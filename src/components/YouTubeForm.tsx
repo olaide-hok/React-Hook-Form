c0731dad4f68f79989ad8794ba141c6cc6f43f18
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-08-06 10:11:28 +0100

    Added Form Validation rules.
    The error messages are populated in the form state object.

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -22,15 +22,42 @@ export const YouTubeForm = () => {
     return (
         <div>
             <h1>YouTube Form ({renderCount / 2})</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
+            <form onSubmit={handleSubmit(onSubmit)} noValidate>
                 <label htmlFor="username">Username</label>
-                <input type="text" id="username" {...register('username')} />
+                <input
+                    type="text"
+                    id="username"
+                    {...register('username', {
+                        required: {
+                            value: true,
+                            message: 'Username is required',
+                        },
+                    })}
+                />
 
                 <label htmlFor="email">E-mail</label>
-                <input type="email" id="email" {...register('email')} />
+                <input
+                    type="email"
+                    id="email"
+                    {...register('email', {
+                        pattern: {
+                            value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*s/,
+                            message: 'Invalid email format',
+                        },
+                    })}
+                />
 
                 <label htmlFor="channel">Channel</label>
-                <input type="text" id="channel" {...register('channel')} />
+                <input
+                    type="text"
+                    id="channel"
+                    {...register('channel', {
+                        required: {
+                            value: true,
+                            message: 'Channel is required',
+                        },
+                    })}
+                />
 
                 <button>Submit</button>
             </form>
