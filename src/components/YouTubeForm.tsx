31403c7e0ff24e7940170c9bf60107461c23406d
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-08-06 11:31:31 +0100

    Fetching input field data via api call

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -11,10 +11,16 @@ type FormValues = {
 
 export const YouTubeForm = () => {
     const form = useForm<FormValues>({
-        defaultValues: {
-            username: 'John Doe',
-            email: '',
-            channel: '',
+        defaultValues: async () => {
+            const response = await fetch(
+                'https://jsonplaceholder.typicode.com/users/1'
+            )
+            const data = await response.json()
+            return {
+                username: '',
+                email: data.email,
+                channel: '',
+            }
         },
     })
     const {register, control, handleSubmit, formState} = form
