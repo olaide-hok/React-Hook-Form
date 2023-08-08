1f732efb92474695bb384298e957366963c239cf
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-08-08 17:36:14 +0100

    Rendered MUI LoginForm.
    Comment out import for App.css file as it overrides MUI css

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import {YouTubeForm} from './components/YouTubeForm'
-import './App.css'
+// import './App.css'
 import {YupYouTubeForm} from './components/YupYouTubeForm'
 import {ZodYouTubeForm} from './components/ZodYouTubeForm'
+import {LoginForm} from './components/MUILoginForm'
 
 function App() {
     return (
         <div>
             {/* <YouTubeForm /> */}
             {/* <YupYouTubeForm /> */}
-            <ZodYouTubeForm />
+            {/* <ZodYouTubeForm /> */}
+            <LoginForm />
         </div>
     )
 }
